4c00fc0cf30e23a42f6de75c5cc08edb5c3fc098
Eben-79 <ebenezerthepirate@gmail.com>
2025-12-26 23:54:12 +0300

    Update Product.cpp

--- src/Product.cpp
+++ src/Product.cpp
@@ -1,1 +1,24 @@
+./include/Product.h
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+
+#include <string>
+
+
+struct Product {
+    int id;
+    std::string name;
+    std::string category;
+    double price;
+    int stock;
+
+
+    Product() : id(0), name(""), category(""), price(0.0), stock(0) {}
+    Product(int id, const std::string &name, const std::string &category, double price, int stock)
+        : id(id), name(name), category(category), price(price), stock(stock) {}
+};
+
+
+#endif
 
